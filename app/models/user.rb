ce7ccc443ae5cc92502addb4707a1bcc92763287
coreyarch1234 <harrilal@mit.edu>
2016-12-13 14:09:45 -0800

    added styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
     validates :city, presence: true
     validates :about, presence: true, length: {maximum: 250}
     validates :password, presence: true, length: {minimum: 8}
-    validates :password_confirmation, presence: true, length: {minimum: 6}
+    validates :password_confirmation, presence: true, length: {minimum: 8}
     VALID_EMAIL_REGEX = /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
     validates :email, presence: true, length: { maximum: 25 },
                     format: { with: VALID_EMAIL_REGEX },
